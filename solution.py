73bada4a26c446613dd177f95653da1bf2713b15
taneka <112857258+MrTaneka@users.noreply.github.com>
2023-04-13 23:11:51 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -3,18 +3,17 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 1119068275 # Ваш chat ID, не меняйте название переменной
+chat_id = 919511341
 
 def solution(control_npv: np.array, test_npv: np.array) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    alpha = 0.01
+    alpha = 0.03
     t_stat, p_val = stats.ttest_ind(control_npv, test_npv, equal_var=True)
 
     if p_val < alpha:
         t = True
     else:
-        t = False   
+        t = False
     return t
-
